8f158d7e91d02968a27f995be12e0346215ca870
Mfxof <matysfreyermuth@gmail.com>
2024-12-02 17:08:30 +0100

    Rajout des affichages textes

--- GeometryDash - Project/Assets/1 - Scripts/UiManager.cs
+++ GeometryDash - Project/Assets/1 - Scripts/UiManager.cs
@@ -6,11 +6,16 @@ using UnityEngine.Audio;
 using UnityEngine.UI;
 using UnityEngine.Animations;
 using System.IO;
+using TMPro;
 
 public class UiManager : MonoBehaviour
 {
     [SerializeField] GameObject shopScreen;
-    // Les texts
+    [SerializeField] TextMeshProUGUI[] inventoryText;
+
+    [Header("Scripts")]
+    [SerializeField] PlayerInventory playerInventory;
+
 
     //-------------------
     //  METHODES DEFAULT
@@ -19,11 +24,14 @@ public class UiManager : MonoBehaviour
     void Start()
     {
         shopScreen.SetActive(false);
+
+        // Placement à revoir
+        textOnDisplay();
     }
 
     void Update()
     {
-        
+
     }
 
     //-------------------
@@ -43,4 +51,12 @@ public class UiManager : MonoBehaviour
     //-------------------
     //  METHODES PRIVEE
     //-------------------
+
+    void textOnDisplay()
+    {
+        inventoryText[0].text = "Argent : " +  playerInventory.gold.ToString();
+        inventoryText[1].text = "Or : " + playerInventory.cash.ToString();
+        inventoryText[2].text = "Etoile : " + playerInventory.stars.ToString();
+        inventoryText[3].text = "Piece etoiles : " + playerInventory.starsCoins.ToString();
+    }
 }
