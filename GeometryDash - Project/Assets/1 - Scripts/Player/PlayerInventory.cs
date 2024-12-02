4ec312baef78951320a057d74f089366438c69b5
Mfxof <matysfreyermuth@gmail.com>
2024-12-02 14:33:55 +0100

    Ajout d'un inventaire pour le joueur

---
+++ GeometryDash - Project/Assets/1 - Scripts/Player/PlayerInventory.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    [Header("Ressources")]
+    public int gold;
+    public int cash;
+    public int stars;
+    public int starCoins;
+
+    [Header("Others")]
+    // Mettre en scriptables
+    public int skins;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
