ef4a99c5672f02e09395e1f44a06a4cb103d3e8c
ssedghi <shamim.sedghi@etu.unistra.fr>
2024-12-09 22:57:02 +0100

    apply the assets UI and the click does not work

---
+++ GeometryDash - Project/Assets/1 - Scripts/community/community.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEngine.Animations;
+using System.IO;
+
+public class CommunityMenuController : MonoBehaviour
+{
+    // Références aux panneaux et autres éléments UI
+    public GameObject communityGlobalPanel;
+
+
+    // Référence à l'écran de chargement
+    public GameObject loadingScreen;
+
+    void Start()
+    {
+        // Assurez-vous que tous les panneaux sont désactivés au démarrage
+        DisableAllPanels();
+        communityGlobalPanel.SetActive(false); // Désactive le panneau communautaire au démarrage
+    }
+
+    void Update()
+    {
+        // Ajoutez ici toute logique supplémentaire nécessaire pendant la mise à jour
+    }
+
+    // Méthode pour activer le panneau communautaire
+    public void ActivateCommunityPanel()
+    {
+        DisableAllPanels();
+        communityGlobalPanel.SetActive(true);
+    }
+
+    // Méthode pour désactiver tous les panneaux
+    private void DisableAllPanels()
+    {
+        communityGlobalPanel.SetActive(false);
+       
+    }
+
+    // Chargez une scène de manière asynchrone
+    public void LoadScene(int sceneIndex)
+    {
+        StartCoroutine(LoadSceneAsync(sceneIndex));
+    }
+
+    // Coroutine pour charger la scène asynchronement
+    private IEnumerator LoadSceneAsync(int sceneIndex)
+    {
+        loadingScreen.SetActive(true);
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!asyncLoad.isDone)
+        {
+            // Optionnel : Mettez à jour une barre de progression ici
+            yield return null;
+        }
+    }
+}
+
