e3e1da2686c5d42cd1906911f6b7ae943fba2605
Mfxof <matysfreyermuth@gmail.com>
2024-12-16 14:09:10 +0100

    Rajout / correction du playerController
    Co-Authored-By: Ricardo ANNIS <123905385+Ricdrum14@users.noreply.github.com>

--- GeometryDash - Project/Assets/1 - Scripts/Player/PlayerController.cs
+++ GeometryDash - Project/Assets/1 - Scripts/Player/PlayerController.cs
@@ -2,17 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum Speed
+{
+    Slow = 0,
+    Normal = 1,
+    Fast = 2,
+    VeryFast = 3,
+    SuperFast = 4,
+}
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Speed CurrentSpeed;
+    //                           0      1      2      3       4                            
+    float[] speedValues = { 8.6f, 10.4f, 12.96f, 15.6f, 19.27f };
+
+    public Transform GroundCheckTransform;
+    public float GroundCheckRadius;
+    public LayerMask GroundMask;
+
+    public Transform Sprite;
+
+    Rigidbody2D rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.position += Vector3.right * speedValues[(int)CurrentSpeed] * Time.deltaTime;
+
+        if (OnGround())
+        {
+
+            if (Input.GetMouseButton(0) || Input.GetKeyDown("space"))
+            {
+                rb.velocity = Vector2.zero;
+                rb.AddForce(Vector2.up * 26.6581f, ForceMode2D.Impulse);
+            }
+        }
+        else
+        {
+            Sprite.Rotate(Vector3.back * 5);
+        }
+    }
+
+    bool OnGround()
+    {
+        return Physics2D.OverlapCircle(GroundCheckTransform.position, GroundCheckRadius, GroundMask);
     }
 }
