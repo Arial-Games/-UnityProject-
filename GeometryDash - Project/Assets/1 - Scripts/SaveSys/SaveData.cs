6210d457f6cdf60a7e5bb8e04fb3889c743e61fb
Mfxof <matysfreyermuth@gmail.com>
2025-01-06 17:03:07 +0100

    Raout de builds, Sauvgarde au Leave du jeu

--- GeometryDash - Project/Assets/1 - Scripts/SaveSys/SaveData.cs
+++ GeometryDash - Project/Assets/1 - Scripts/SaveSys/SaveData.cs
@@ -1,7 +1,8 @@
-using System.IO;
-using UnityEngine;
-using TMPro;
 using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using UnityEngine;
 
 public class SaveData : MonoBehaviour
 {
@@ -9,6 +10,12 @@ public class SaveData : MonoBehaviour
     [SerializeField] PlayerInventory _inventory;
 
     string saveSeparator = "%DATA%";
+    string encryptionKey = "^D_N=G^$SHK6k_1PP#4ocH@=o)2cDaNQ"; // Clé de 32 caractères
+
+
+    //-------------------
+    //  METHODES DEFAULT
+    //-------------------
 
     void Awake()
     {
@@ -20,21 +27,24 @@ public class SaveData : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            save();
-        }
+        //if (Input.GetKeyDown(KeyCode.S))
+        //{
+        //    save();
+        //}
 
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            load();
-        }
-
+        //if (Input.GetKeyDown(KeyCode.C))
+        //{
+        //    load();
+        //}
     }
+
+
+    //-------------------
+    //  METHODES PUBLIC
+    //-------------------
 
     public void save()
     {
-
         bool[] skins = playerStat.possesionId;
         string skinsString = string.Join(",", skins);
 
@@ -51,21 +61,25 @@ public class SaveData : MonoBehaviour
             playerStat.openSave.ToString(),
 
             skinsString
-
-            // Rajoute la save des skins
-    };
+        };
 
         string saveString = string.Join(saveSeparator, content);
 
-        File.WriteAllText(Application.dataPath + "/data.txt", saveString);
+        // Chiffrement des données
+        string encryptedData = Encrypt(saveString, encryptionKey);
+
+        File.WriteAllText(Application.dataPath + "/data.txt", encryptedData);
         Debug.Log("Sauvegarde OK");
     }
 
     public void load()
     {
-        if (File.Exists("/data.txt"))
+        if (File.Exists(Application.dataPath + "/data.txt"))
         {
-            string saveString = File.ReadAllText(Application.dataPath + "/data.txt");
+            string encryptedData = File.ReadAllText(Application.dataPath + "/data.txt");
+
+            // Déchiffrement des données
+            string saveString = Decrypt(encryptedData, encryptionKey);
 
             string[] content = saveString.Split(new[] { saveSeparator }, System.StringSplitOptions.None);
             playerStat.gold = float.Parse(content[0]);
@@ -78,20 +92,59 @@ public class SaveData : MonoBehaviour
             playerStat.storyProgression = int.Parse(content[6]);
             playerStat.openSave = bool.Parse(content[7]);
 
-            // Rajoute le load des skins
             string skinsString = content[8];
-            string[] skins = skinsString.Split(','); // Sépare la chaîne pour obtenir un tableau de strings
+            string[] skins = skinsString.Split(',');
 
-            // Convertit les strings en bool et les assigne à playerStat.possesionId
             playerStat.possesionId = Array.ConvertAll(skins, bool.Parse);
-
-
 
             _inventory.saveCall();
 
             Debug.Log("Chargement OK");
         }
-        Debug.Log("Aucune sauvegarde");
+        else
+        {
+            Debug.Log("Aucune sauvegarde");
+        }
     }
 
+
+    //-------------------
+    //  METHODES PRIVEE
+    //-------------------
+
+    // Méthode de chiffrement AES
+    private string Encrypt(string plainText, string key)
+    {
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = Encoding.UTF8.GetBytes(key);
+            aes.IV = new byte[16]; // Vecteur d'initialisation à 0 (simple mais pas recommandé pour production)
+
+            using (var encryptor = aes.CreateEncryptor(aes.Key, aes.IV))
+            {
+                byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
+                byte[] encryptedBytes = encryptor.TransformFinalBlock(plainBytes, 0, plainBytes.Length);
+
+                return System.Convert.ToBase64String(encryptedBytes);
+            }
+        }
+    }
+
+    // Méthode de déchiffrement AES
+    private string Decrypt(string cipherText, string key)
+    {
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = Encoding.UTF8.GetBytes(key);
+            aes.IV = new byte[16]; // Même vecteur d'initialisation que pour le chiffrement
+
+            using (var decryptor = aes.CreateDecryptor(aes.Key, aes.IV))
+            {
+                byte[] cipherBytes = System.Convert.FromBase64String(cipherText);
+                byte[] plainBytes = decryptor.TransformFinalBlock(cipherBytes, 0, cipherBytes.Length);
+
+                return Encoding.UTF8.GetString(plainBytes);
+            }
+        }
+    }
 }
