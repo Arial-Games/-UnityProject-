31f43ec3bc7585e9bdd4bd52824d04178a64668e
Mfxof <matysfreyermuth@gmail.com>
2025-01-06 15:14:19 +0100

    Système de dialogue

---
+++ GeometryDash - Project/Assets/7 - Assets/---TEMP---/Scripts/Dialogue.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Dialogue 1", menuName = "Dialogue", order = 1)]
+
+public class Dialogue : ScriptableObject
+{
+	public string[] CharactersList;
+	public List<DialogueElement> DialogueItems;
+}
+
