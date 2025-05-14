5e8b0a6cc57143378dadcc69b93d9366168c773d
Mfxof <matysfreyermuth@gmail.com>
2025-05-14 12:10:04 +0200

    VEB0.4.5 - Finalisation Workshop

--- GeometryDash - Project/Assets/1 - Scripts/LevelLoader/LevelStorageJson.cs
+++ GeometryDash - Project/Assets/1 - Scripts/LevelLoader/LevelStorageJson.cs
@@ -13,13 +13,15 @@ public class LevelEditor : MonoBehaviour
     [SerializeField] private Transform paletteContainer;     // Le conteneur UI pour les icônes d'objets (ex: ObjectPlacer - Panel)
     [SerializeField] private GameObject paletteButtonPrefab; // Le prefab du bouton d'icône à instancier pour chaque objet
     [SerializeField] private SO_WorkshopObjects[] so_WorkshopObjects;
-        private SO_WorkshopObjects currentSO = null;
-        private int currentFileIndex = 1;
- // Flag pour autoriser ou non les modifications
+    private SO_WorkshopObjects currentSO = null;
+    private int currentFileIndex = 1;
+    // Flag pour autoriser ou non les modifications
     private bool isEditing = true;
 
-    private List<Placeable> placed = new List<Placeable>(); 
-     void Start()
+    private List<Placeable> placed = new List<Placeable>();
+
+    [Header("Script"), SerializeField] StartScene startScene_sc;
+    void Start()
     {
         Debug.Log($"▶ LevelEditor.Start() – so_WorkshopObjects.Length = {so_WorkshopObjects.Length}");
         Debug.Log($"▶ paletteButtonPrefab = {paletteButtonPrefab}");
@@ -27,7 +29,7 @@ public class LevelEditor : MonoBehaviour
 
         for (int i = 0; i < so_WorkshopObjects.Length; i++)
         {
-       SO_WorkshopObjects so = so_WorkshopObjects[i];
+            SO_WorkshopObjects so = so_WorkshopObjects[i];
             Debug.Log($" • Création bouton pour « {so.typeName} »");
 
             if (paletteButtonPrefab == null || paletteContainer == null)
@@ -47,14 +49,14 @@ public class LevelEditor : MonoBehaviour
                 Debug.LogError($"❌ Pas de Button sur {btnObj.name}");
                 continue;
             }
-            
+
             int index = i;
             btn.onClick.AddListener(() => SelectObject(index));
 
 
         }
     }
-public void SelectObject(int i)
+    public void SelectObject(int i)
     {
         if (i >= 0 && i < so_WorkshopObjects.Length)
         {
@@ -63,7 +65,7 @@ public void SelectObject(int i)
         }
     }
 
-    public void ButtonData(int i )
+    public void ButtonData(int i)
     {
         StartCoroutine(Fade(i));
     }
@@ -78,13 +80,13 @@ public void SelectObject(int i)
         yield return new WaitForSeconds(2f);
 
 
-        
-               // currentSO = so[so.uniqueId];
-                
+
+        // currentSO = so[so.uniqueId];
+
     }
 
     void Update()
-    {    
+    {
         // Si non modifiable, ne rien faire
         if (!isEditing) return;
 
@@ -94,95 +96,97 @@ public void SelectObject(int i)
         }
 
         if (currentSO != null && Input.GetMouseButtonDown(0))
-{
- 
-if (currentSO.typeName == "EndWall")
-{
-    foreach (Transform t in sceneRoot.transform)
-        if (t.name.Contains("EndWall")) { Destroy(t.gameObject); break; }
+        {
 
-    placed.RemoveAll(p => p.type == "EndWall");
-     //go.transform.rotation = Quaternion.Euler(0, 0, 90);
-}
+            if (currentSO.typeName == "EndWall")
+            {
+                foreach (Transform t in sceneRoot.transform)
+                    if (t.name.Contains("EndWall")) { Destroy(t.gameObject); break; }
 
-    Vector3 mousePos = Input.mousePosition;
-    float distance = Mathf.Abs(Camera.main.transform.position.z);
-    Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, distance));
-    pos.z = 0;
-   
+                placed.RemoveAll(p => p.type == "EndWall");
+                //go.transform.rotation = Quaternion.Euler(0, 0, 90);
+            }
 
-    var go = Instantiate(currentSO.prefab, pos, Quaternion.identity, sceneRoot.transform);
+            Vector3 mousePos = Input.mousePosition;
+            float distance = Mathf.Abs(Camera.main.transform.position.z);
+            Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, distance));
+            pos.z = 0;
 
-    placed.Add(new Placeable {
-        type = currentSO.typeName,
-        x    = pos.x,
-        y    = pos.y,
-        rot  = go.transform.eulerAngles.z,
-        sx   = go.transform.localScale.x,
-        sy   = go.transform.localScale.y
-    });
-}
+
+            var go = Instantiate(currentSO.prefab, pos, Quaternion.identity, sceneRoot.transform);
+
+            placed.Add(new Placeable
+            {
+                type = currentSO.typeName,
+                x = pos.x,
+                y = pos.y,
+                rot = go.transform.eulerAngles.z,
+                sx = go.transform.localScale.x,
+                sy = go.transform.localScale.y
+            });
+        }
     }
- public void ExportJsonOnly()
-{
-    // On vérifie qu'il y a bien des objets placés avant d'exporter
-    if (placed.Count == 0)
+    public void ExportJsonOnly()
     {
-        Debug.LogWarning("Aucun objet placé. Le JSON ne peut pas être créé.");
-        return;
-    }
-    
-    var data = new LevelData { objects = placed };
-    string json = JsonUtility.ToJson(data, true);
-    
-    string directory = Application.dataPath + "/8 - Data";
-    string baseFileName = "level_";
-    
-    while (File.Exists($"{directory}/{baseFileName}{currentFileIndex}.json"))
-    {
-        currentFileIndex++;
-    }
-    
-    string path = $"{directory}/{baseFileName}{currentFileIndex}.json";
-    if (currentSO.typeName=="Player" && placed.Exists(p=>p.type=="Player"))
-{
-    Debug.Log("Player déjà placé"); return;
-}
+        // On vérifie qu'il y a bien des objets placés avant d'exporter
+        if (placed.Count == 0)
+        {
+            Debug.LogWarning("Aucun objet placé. Le JSON ne peut pas être créé.");
+            return;
+        }
 
-    Directory.CreateDirectory(directory);
-    File.WriteAllText(path, json);
-    Debug.Log($"✅ JSON exporté → {path}");
-    
-    // Une fois sauvegardé, le placement est désactivé pour éviter la modification de la version sauvegardée.
-    isEditing = false;
-    
-    // Vous pouvez aussi vider placed si vous ne souhaitez plus accumuler,
-    //placed.Clear();
-}
+        var data = new LevelData { objects = placed };
+        string json = JsonUtility.ToJson(data, true);
 
-public void TestLevel()
-{
-    // Ici, on ne fait PAS d'export JSON, afin d'éviter de créer un fichier vide.
-    // Si aucun objet n'a été placé, on affiche un message d'avertissement.
-    if (placed.Count == 0)
-    {
-        Debug.LogWarning("Impossible de tester le niveau : aucun objet n'a été placé.");
-        return;
+        string directory = Application.dataPath + "/8 - Data";
+        string baseFileName = "level_";
+
+        while (File.Exists($"{directory}/{baseFileName}{currentFileIndex}.json"))
+        {
+            currentFileIndex++;
+        }
+
+        string path = $"{directory}/{baseFileName}{currentFileIndex}.json";
+        if (currentSO.typeName == "Player" && placed.Exists(p => p.type == "Player"))
+        {
+            Debug.Log("Player déjà placé"); return;
+        }
+
+        Directory.CreateDirectory(directory);
+        File.WriteAllText(path, json);
+        Debug.Log($"✅ JSON exporté → {path}");
+
+        // Une fois sauvegardé, le placement est désactivé pour éviter la modification de la version sauvegardée.
+        isEditing = false;
+
+        // Vous pouvez aussi vider placed si vous ne souhaitez plus accumuler,
+        //placed.Clear();
     }
-    
-    // Désactive l'édition pour éviter d'ajouter d'autres objets lors du test
-    isEditing = false;
-    
-    if (GameJson.Instance == null)
+
+    public void TestLevel()
     {
-        Debug.LogError("GameJson instance not found! Créez un GameObject avec le composant GameJson.");
-        return;
+        // Ici, on ne fait PAS d'export JSON, afin d'éviter de créer un fichier vide.
+        // Si aucun objet n'a été placé, on affiche un message d'avertissement.
+        if (placed.Count == 0)
+        {
+            Debug.LogWarning("Impossible de tester le niveau : aucun objet n'a été placé.");
+            return;
+        }
+
+        // Désactive l'édition pour éviter d'ajouter d'autres objets lors du test
+        isEditing = false;
+
+        if (GameJson.Instance == null)
+        {
+            Debug.LogError("GameJson instance not found! Créez un GameObject avec le composant GameJson.");
+            return;
+        }
+
+        // On utilise currentFileIndex qui correspond au dernier niveau sauvegardé (ou à sauvegarder)
+        GameJson.Instance.SetLevelToLoad(currentFileIndex);
+
+        startScene_sc.StartButton(13);
     }
-    
-    // On utilise currentFileIndex qui correspond au dernier niveau sauvegardé (ou à sauvegarder)
-    GameJson.Instance.SetLevelToLoad(currentFileIndex);
-    UnityEngine.SceneManagement.SceneManager.LoadScene(12); 
-}
 }
 
 // Classe pour décrire un objet placé (pour la sérialisation JSON)
